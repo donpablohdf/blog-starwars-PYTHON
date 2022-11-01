592cd5b85ecb0a5cde2488fd68f8c083c32ae951
donPabloHDF <donpabloeldefelicitas@gmail.com>
2022-11-01 20:18:24 +0100

    subir logo

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
 	console.log(variables)
 	return (
 		<nav className="navbar navbar-light bg-secondary text-bg-secondary mb-3 px-3 d-flex">
-			<div className="pl-5"><Link to="/"><img src="https://github.com/donpablohdf/blog-starwars/blob/master/src/img/starWarsLogo.png" height={50} /></Link></div>
+			<div className="pl-5"><Link to="/"><img src="https://logos-download.com/wp-content/uploads/2016/09/Star_Wars_logo-1.png" height={50} /></Link></div>
 			<div className="derecha">
 				<div className=""><Link to="/"><button className="btn btn-primary">Ver componente Home</button></Link></div>
 				<div className="ml-auto">Favoritos</div>
